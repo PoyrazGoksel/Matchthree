49055134c993de0d22450bcf08d2df6d84f7fc11
PoyrazGoksel <55000408+PoyrazGoksel@users.noreply.github.com>
2024-08-12 08:30:50 +0300

    patch to prev

--- Assets/Scripts/Services/ToBeToAPI.cs
+++ Assets/Scripts/Services/ToBeToAPI.cs
@@ -5,6 +5,7 @@ namespace Services
 {
     public class ToBeToAPI
     {
+        private static bool _currentGroup;
         private const float ABGroupChance = 0.5f;
         private const string ABTestPrefKey = "ABTest";
         public static ToBeToAPI Ins{get;private set;}
@@ -18,18 +19,25 @@ namespace Services
 
             bool ab = randomABGroup > ABGroupChance;
 
+            _currentGroup = ab;
+
             if(PlayerPrefs.HasKey(ABTestPrefKey) == false)
             {
-                PlayerPrefs.SetInt(ABTestPrefKey, ab.ToInt());
+                PlayerPrefs.SetInt(ABTestPrefKey, _currentGroup.ToInt());
 
                 Debug.LogWarning("ABTest Init");
             }
             else
             {
-                ab = PlayerPrefs.GetInt(ABTestPrefKey).ToBool();
+                _currentGroup = PlayerPrefs.GetInt(ABTestPrefKey).ToBool();
             }
 
-            Debug.LogWarning($"AB Group: {ab.ToInt()}");
+            Debug.LogWarning($"AB Group: {_currentGroup.ToInt()}");
+        }
+
+        public int GetGroup()
+        {
+            return _currentGroup.ToInt();
         }
     }
 }
